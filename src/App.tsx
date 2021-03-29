a52eb61a039bab05488fd08f2cf8a743d2dd409f
Jss2k <cliffsclifus@icloud.com>
2021-03-30 01:32:48 +0300

    preloader svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import Loader from "./assets/loader"
+import Preloader from "./assets/preloader"
 import './index.css'
 
 const App = () => {
 
   return (
     <div className="App" >
-      <div style={{
+      <button style={{
         height: '40px',
         background: 'red',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        position: 'relative'
       }}>
-        <span style={{
-          display: 'block',
-        marginTop: '-5px',
-        }}>
-          <Loader />
-          </span>
+
+          <Preloader />
+
         <span style={{
         margin: '0 10px',
-        lineHeight: '32px',
+        lineHeight: '40px',
         // verticalAlign: 'middle',
         // display: 'inline-block'
       }}>
           какой-то текст
         </span>
-      </div>
+      </button>
     </div>
   )
 }
