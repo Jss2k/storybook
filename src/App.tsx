12c9a56c3d660f0307cf2c510435c35a3c289925
Jss2k <cliffsclifus@icloud.com>
2021-03-30 00:30:37 +0300

    loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import Task from "./components/Task/Task"
+import Loader from "./assets/loader"
+import './index.css'
 
-const Taska = {
-  id: '2',
-  title: 'dsfsdf'
-}
-
-function App() {
+const App = () => {
 
   return (
     <div className="App" >
-      <Task task={Taska}/>
+      <div style={{
+        height: '40px',
+        background: 'red',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative'
+      }}>
+        <span style={{
+          display: 'block',
+        marginTop: '-5px',
+        }}>
+          <Loader />
+          </span>
+        <span style={{
+        margin: '0 10px',
+        lineHeight: '32px',
+        // verticalAlign: 'middle',
+        // display: 'inline-block'
+      }}>
+          какой-то текст
+        </span>
+      </div>
     </div>
   )
 }
